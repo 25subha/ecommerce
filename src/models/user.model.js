238bd0289420257b27519b72604f09fa7b354760
subhankar <mrsubhankar25@gmail.com>
2025-03-05 20:50:45 +0530

    define userSchema end point

---
+++ src/models/user.model.js
@@ -0,0 +1,26 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+    {
+        fullName : {
+            type: String, 
+            required: true
+        },
+        userName: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true
+        }
+    },
+    {
+    timestamps: true
+});
+
+export const User =  mongoose.model("User",userSchema ) 
