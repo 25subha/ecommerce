a29118d984580ff5a86905522cd4a84ba9b0693d
subhankar <mrsubhankar25@gmail.com>
2025-03-05 20:57:44 +0530

    define productSchema endpoint

---
+++ src/models/product.model.js
@@ -0,0 +1,23 @@
+import mongoose, { Schema } from "mongoose";
+
+const productSchema = new Schema ({
+    title: {
+        type: String,
+        required: true
+    }, 
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    imageUrl: {
+        type: String,
+        required: true
+    }
+    
+}, { timestamps: true});
+
+export const Product = mongoose.model("Product", productSchema);
